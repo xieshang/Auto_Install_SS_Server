7c379c4c4752962ee5a751e9c62add05f0a25a89
xieshang <xieshang@f1802104-921a-4fed-af06-11301af9d76a>
2016-12-14 08:47:06 +0000

    [完成] 1、建立采集命令规则完成 2、实现： OpenSite|Html_SetPoint|Html_GetBetween|DGV_AddLine 3、测试获取第一条数据成功 [下一步任务] 1、文件读写接口； 2、文件拖动添加至列表功能；

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,6 +22,8 @@ Partial Class Form1
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.log = New System.Windows.Forms.RichTextBox()
         Me.reg = New System.Windows.Forms.Button()
         Me.login = New System.Windows.Forms.Button()
@@ -30,17 +32,33 @@ Partial Class Form1
         Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.Public_SS = New System.Windows.Forms.TabPage()
+        Me.ssr_dgv = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.arukas_io = New System.Windows.Forms.TabPage()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column10 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ProgressBar = New System.Windows.Forms.ToolStripProgressBar()
         CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.TabControl1.SuspendLayout()
+        Me.Public_SS.SuspendLayout()
+        CType(Me.ssr_dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.arukas_io.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'log
         '
-        Me.log.Location = New System.Drawing.Point(1, 243)
+        Me.log.Location = New System.Drawing.Point(1, 459)
         Me.log.Name = "log"
-        Me.log.Size = New System.Drawing.Size(286, 139)
+        Me.log.Size = New System.Drawing.Size(696, 139)
         Me.log.TabIndex = 0
         Me.log.Text = ""
         '
@@ -66,6 +84,9 @@ Partial Class Form1
         '
         Me.DGV.AllowUserToAddRows = False
         Me.DGV.AllowUserToDeleteRows = False
+        Me.DGV.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
         Me.DGV.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2})
         Me.DGV.Location = New System.Drawing.Point(3, 34)
@@ -74,7 +95,7 @@ Partial Class Form1
         Me.DGV.RowHeadersWidth = 4
         Me.DGV.RowTemplate.Height = 27
         Me.DGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
-        Me.DGV.Size = New System.Drawing.Size(277, 176)
+        Me.DGV.Size = New System.Drawing.Size(682, 176)
         Me.DGV.TabIndex = 3
         '
         'Column1
@@ -82,7 +103,7 @@ Partial Class Form1
         Me.Column1.HeaderText = "应用名称"
         Me.Column1.Name = "Column1"
         Me.Column1.ReadOnly = True
-        Me.Column1.Width = 200
+        Me.Column1.Width = 435
         '
         'Column2
         '
@@ -98,18 +119,80 @@ Partial Class Form1
         Me.TabControl1.Location = New System.Drawing.Point(1, 2)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(290, 241)
+        Me.TabControl1.Size = New System.Drawing.Size(696, 456)
         Me.TabControl1.TabIndex = 4
         '
         'Public_SS
         '
+        Me.Public_SS.Controls.Add(Me.ssr_dgv)
+        Me.Public_SS.Controls.Add(Me.Button1)
+        Me.Public_SS.Controls.Add(Me.DataGridView1)
         Me.Public_SS.Location = New System.Drawing.Point(4, 25)
         Me.Public_SS.Name = "Public_SS"
         Me.Public_SS.Padding = New System.Windows.Forms.Padding(3)
-        Me.Public_SS.Size = New System.Drawing.Size(282, 212)
+        Me.Public_SS.Size = New System.Drawing.Size(688, 427)
         Me.Public_SS.TabIndex = 0
         Me.Public_SS.Text = "公布SS"
         Me.Public_SS.UseVisualStyleBackColor = True
+        '
+        'ssr_dgv
+        '
+        Me.ssr_dgv.AllowUserToAddRows = False
+        Me.ssr_dgv.AllowUserToDeleteRows = False
+        Me.ssr_dgv.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("宋体", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.ssr_dgv.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.ssr_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.ssr_dgv.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column6, Me.Column7, Me.Column8, Me.Column9, Me.Column10})
+        Me.ssr_dgv.Location = New System.Drawing.Point(3, 212)
+        Me.ssr_dgv.Name = "ssr_dgv"
+        Me.ssr_dgv.ReadOnly = True
+        Me.ssr_dgv.RowHeadersWidth = 4
+        Me.ssr_dgv.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        Me.ssr_dgv.RowTemplate.Height = 27
+        Me.ssr_dgv.Size = New System.Drawing.Size(682, 212)
+        Me.ssr_dgv.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(349, 98)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("宋体", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle2
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column3, Me.Column5})
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 3)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersWidth = 4
+        Me.DataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        Me.DataGridView1.RowTemplate.Height = 27
+        Me.DataGridView1.Size = New System.Drawing.Size(682, 206)
+        Me.DataGridView1.TabIndex = 0
         '
         'arukas_io
         '
@@ -119,16 +202,84 @@ Partial Class Form1
         Me.arukas_io.Location = New System.Drawing.Point(4, 25)
         Me.arukas_io.Name = "arukas_io"
         Me.arukas_io.Padding = New System.Windows.Forms.Padding(3)
-        Me.arukas_io.Size = New System.Drawing.Size(282, 212)
+        Me.arukas_io.Size = New System.Drawing.Size(688, 427)
         Me.arukas_io.TabIndex = 1
         Me.arukas_io.Text = "arukas.io"
         Me.arukas_io.UseVisualStyleBackColor = True
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "采集规则"
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        Me.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.Column3.Width = 595
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "状态"
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        Me.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.Column5.Width = 80
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "服务器IP"
+        Me.Column6.Name = "Column6"
+        Me.Column6.ReadOnly = True
+        Me.Column6.Width = 180
+        '
+        'Column7
+        '
+        Me.Column7.FillWeight = 50.0!
+        Me.Column7.HeaderText = "端口"
+        Me.Column7.Name = "Column7"
+        Me.Column7.ReadOnly = True
+        Me.Column7.Width = 80
+        '
+        'Column8
+        '
+        Me.Column8.FillWeight = 80.0!
+        Me.Column8.HeaderText = "密码"
+        Me.Column8.Name = "Column8"
+        Me.Column8.ReadOnly = True
+        Me.Column8.Width = 120
+        '
+        'Column9
+        '
+        Me.Column9.FillWeight = 80.0!
+        Me.Column9.HeaderText = "加密"
+        Me.Column9.Name = "Column9"
+        Me.Column9.ReadOnly = True
+        '
+        'Column10
+        '
+        Me.Column10.HeaderText = "注释"
+        Me.Column10.Name = "Column10"
+        Me.Column10.ReadOnly = True
+        Me.Column10.Width = 195
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ProgressBar})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 600)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(700, 22)
+        Me.StatusStrip1.TabIndex = 5
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.Size = New System.Drawing.Size(100, 16)
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(288, 382)
+        Me.ClientSize = New System.Drawing.Size(700, 622)
+        Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.log)
         Me.Controls.Add(Me.TabControl1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
@@ -138,18 +289,36 @@ Partial Class Form1
         Me.Text = "arukas.io Tools"
         CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
         Me.TabControl1.ResumeLayout(False)
+        Me.Public_SS.ResumeLayout(False)
+        CType(Me.ssr_dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.arukas_io.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents log As System.Windows.Forms.RichTextBox
     Friend WithEvents reg As System.Windows.Forms.Button
     Friend WithEvents login As System.Windows.Forms.Button
     Friend WithEvents DGV As System.Windows.Forms.DataGridView
-    Friend WithEvents Column1 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
     Friend WithEvents Public_SS As System.Windows.Forms.TabPage
     Friend WithEvents arukas_io As System.Windows.Forms.TabPage
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Column1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ssr_dgv As System.Windows.Forms.DataGridView
+    Friend WithEvents Column6 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column9 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column10 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ProgressBar As System.Windows.Forms.ToolStripProgressBar
 
 End Class
